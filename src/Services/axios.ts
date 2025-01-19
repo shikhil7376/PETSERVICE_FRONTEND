dabdc310d304f6096777c4ac829fdbbce5843ec9
shikhil <shikhilkrishnan2018@gmail.com>
2025-01-19 12:51:13 +0530

    put the backend url direcly in axios for check

--- src/Services/axios.ts
+++ src/Services/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import errorHandle from "../Api/Error";
 
 
-const BASE_URL =import.meta.env.VITE_BASE_URL 
+const BASE_URL ='https://quiix.shop/api'
 
 const api = axios.create({
   baseURL: BASE_URL, 
